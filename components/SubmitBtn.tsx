81c7c6e344eef8042ca4bb68c0653e55fdfe6452
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-08-28 19:30:56 -0400

    Fix Submit Button

--- components/SubmitBtn.tsx
+++ components/SubmitBtn.tsx
@@ -9,7 +9,7 @@ const SubmitBtn = () => {
   return (
     <button className="text-white bg-mintbg md:w-1/5 text-[30px] sm:w-full tracking-wide rounded-[50px] md:h-full sm:h-[100px]" disabled={pending}>
         { pending ? (
-            <div className="h-12 w-12 animate-spin rounded-full mx-auto text-center border-b-4 border-bluebg"></div>
+            <div className="h-12 w-12 animate-spin rounded-full mx-auto text-center border-b-4 border-bluebg mx-auto"></div>
         ): (
             <>
                 Submit
