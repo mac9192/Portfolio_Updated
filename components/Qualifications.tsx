35f3ee5a38335135e46038499782392de02f8bb5
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-09-02 12:00:51 -0400

    Edited Services Section/Re-Arranged Elements

--- components/Qualifications.tsx
+++ components/Qualifications.tsx
@@ -7,7 +7,7 @@ const Qualifications = () => {
     <div className="flex flex-col   bg-bluebg m-auto text-center min-h-[640px] md:mt-[-120px] sm:mt-[-80px]">
         <div className="md:container md:mx-auto py-10 mx-auto py-[60px]">
              <h2 className="text-[48px] tracking-wide text-white m-auto sm:p-5">Hello, I am Miguel. Nice to meet you.</h2>
-        <p className="md:text-[30px] sm:text-[20px]  md:p-10 sm:p-5 tracking-wide text-white text-center sm:mb-4">Hey there! I'm a tech enthusiast with experience in blockchain and web development, alongside a knack for design. I've collaborated with startups, agencies, and established businesses, and I'm driven by curiosity and a love for coding. When I'm not working on projects, I'm busy sharpening my coding skills and pursuing personal ventures. Let's team up for your next tech adventure!</p>
+        <p className="md:text-[30px] sm:text-[20px]  md:p-10 sm:p-5 tracking-wide text-white text-center sm:mb-4">Hey there! I am a tech enthusiast with experience in blockchain and web development, alongside a knack for design. I have collaborated with startups, agencies, and established businesses, and I am driven by curiosity and a love for coding. When I am not working on projects, I am busy sharpening my coding skills and pursuing personal ventures. Lets team up for your next tech adventure!</p>
         </div>
        
     </div>
