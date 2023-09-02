f24d274f5107924c2fa7b9d10cd80578937fc628
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-09-02 00:23:29 -0400

    Added new SVG Icons

--- components/Services.tsx
+++ components/Services.tsx
@@ -4,10 +4,10 @@ import { PROJECT_QUALIFICATIONS } from '../PROJECT_DATA'
 
 const Services = () => {
 
-  const qualifications1 = PROJECT_QUALIFICATIONS[0]
-  const qualifications2 = PROJECT_QUALIFICATIONS[1]
-  const qualifications3 = PROJECT_QUALIFICATIONS[2]
-  console.log(qualifications1.skills)
+  const qualifications1 = PROJECT_QUALIFICATIONS[0].skills 
+  const qualifications2 = PROJECT_QUALIFICATIONS[1].skills
+  const qualifications3 = PROJECT_QUALIFICATIONS[2].skills
+  console.log(qualifications1)
    
 
   return (
@@ -37,18 +37,23 @@ const Services = () => {
      <div className="flex flex-col mx-auto text-center items-center tracking-wide">
           
        
-               <Image src = "/assets/www.png" alt="/" width="150" height="500" priority={true} /> 
+               <Image className="p-3" src = "/assets/icon1.svg" alt="/" width="100" height="500" priority={true} /> 
                 <div className="text-4xl">Designer</div>
-                <div className="p-7 text-2xl sm:text-xl">I value quality within content structure, clean and inviting designs.</div>
+                <div className="p-7 text-2xl sm:text-xl">I value quality within content structure, clean and inviting designs and designs and design.</div>
                 <div className="p-2 text-2xl text-bluebg">Things I enjoy designing:</div>
                 <div className="pb-5 text-2xl sm:text-xl">UX, UI, Web, Apps, Logos</div>
                 <div className="p-5 text-2xl text-bluebg ">Design Tools:</div>
                 <div className="text-2xl">
-                <div className="p-1 sm:text-xl">Figma</div>
-                <div className="p-1 sm:text-xl">Adobe</div>
-                <div className="p-1 sm:text-xl">PhotoShop</div>
-                <div className="p-1 sm:text-xl">Font Awesome</div>
-                <div className="p-1 sm:text-xl">Pen & Paper</div>
+             
+            
+                <ul>{qualifications1.map((qualification, index)=> (
+                    <li key={index} className="flex flex-col gap-x-3 y-3">
+                      <div className=""> {qualification} </div> 
+                    </li>
+                  ))
+                }
+                </ul>
+      
                
 
                 </div>
@@ -57,24 +62,20 @@ const Services = () => {
       </div>
       <div className="w-1/3 bg-white  border-r   border border-l border-b w-full  ">
       <div className="flex flex-col mx-auto text-center items-center tracking-wide  ">
-               <Image src = "/assets/www.png" alt="/" width="150" height="500" priority={true} /> 
+               <Image className="p-3" src = "/assets/icon2.svg" alt="/" width="100" height="500" priority={true} /> 
                 <div className="text-4xl">Frontend Developer</div>
                 <div className="p-7 text-2xl sm:text-xl">I like to code things from scratch and enjoy watching designs come to life on the browser.</div>
                 <div className="p-2 text-2xl text-bluebg">Languages I Speak:</div>
                 <div className="pb-5 text-2xl sm:text-xl">JavaScript, TypeScript HTML, CSS</div>
                 <div className="p-5 text-2xl text-bluebg">Dev Tools:</div>
-                <div className="text-2xl">
-                <div className="p-1 sm:text-xl">Nextjs</div>
-                <div className="p-1 sm:text-xl">Proof-of-Stake</div>
-                <div className="p-1 sm:text-xl">React</div>
-                <div className="p-1 sm:text-xl">Node</div>
-                <div className="p-1 sm:text-xl">Express</div>
-                <div className="p-1 sm:text-xl">MongoDB</div>
-                <div className="p-1 sm:text-xl">GraphQL</div>
-                <div className="p-1 sm:text-xl">Directus</div>
-                <div className="p-1 sm:text-xl">Sanity</div>
-                <div className="p-1 sm:text-xl">Tailwind</div>
-                <div className="p-1 sm:text-xl">Framer</div>
+                <div className="text-xl">
+                <ul>{qualifications2.map((qualification, index)=> (
+                    <li key={index} className="flex flex-col gap-x-3 y-3">
+                      <div className=""> {qualification} </div> 
+                    </li>
+                  ))
+                }
+                </ul>
 
                 </div>
                 
@@ -82,23 +83,20 @@ const Services = () => {
       </div>
       <div className="w-1/3 bg-white md:rounded-r-[50px] border   border-b border-r w-full   ">
             <div className="flex flex-col mx-auto text-center items-center tracking-wide   h-full">
-               <Image src = "/assets/www.png" alt="/" width="150" height="500" priority={true} /> 
+               <Image className="p-3"src = "/assets/icon3.svg" alt="/" width="100" height="500" priority={true} /> 
                 <div className="text-4xl">Block Chain</div>
                 <div className="p-7 text-2xl sm:text-xl">Navigating the Cosmos: My Journey in Powering the Future of Blockchain through Proof of Stake.</div>
                 <div className="p-2 text-2xl text-bluebg">Things I enjoy in blockchain:</div>
                 <div className="pb-5 text-2xl sm:text-xl">UX, UI, Web, Apps, Logos</div>
                 <div className="p-5 text-2xl text-bluebg">Blockchain Tools:</div>
                 <div className="text-2xl">
-                <div className="p-1 text-2xl sm:text-xl">Cosmos Ecosystem</div>
-                <div className="p-1 sm:text-xl">Proof-of-Stake</div>
-                <div className="p-1 sm:text-xl">Tendermint</div>
-                <div className="p-1 sm:text-xl">Rust</div>
-                <div className="p-1 sm:text-xl">CosmWasm</div>
-                <div className="p-1 sm:text-xl">Validator</div>
-                <div className="p-1 sm:text-xl">Linux Ubuntu</div>
-                <div className="p-1 sm:text-xl">AWS EC2</div>
-                <div className="p-1 sm:text-xl">Linode</div>
-                <div className="p-1 sm:text-xl">Nginx</div>
+                <ul>{qualifications2.map((qualification, index)=> (
+                    <li key={index} className="flex flex-col gap-x-3 y-3">
+                      <div className=""> {qualification} </div> 
+                    </li>
+                  ))
+                }
+                </ul>
 
                 </div>
                 
