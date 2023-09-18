c794e99ad337572b7574495d0799734187bf6954
Miguel Colmenares <miguelcolmenares96@gmail.com>
2023-09-18 14:11:51 -0400

    Resume Update

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,7 +7,7 @@ const Resume = () => {
     <div className=" bg-purplebg">
             <div className="min-w-full   md:container md:mx-auto p-14 ">
             <h2 className="text-center mx-auto sm:text-3xl md:text-[60px] font-medium title-font text-white  lg:mb-0 mb-4 pb-8">My Current Resume</h2>
-        <iframe className="w-full h-screen p-3" src="/assets/resume1.pdf" frameBorder="100"></iframe>
+        <iframe className="w-full h-screen p-3" src="/assets/miguelcolmenares_resume1.pdf" frameBorder="100"></iframe>
     </div> 
     </div>
 
